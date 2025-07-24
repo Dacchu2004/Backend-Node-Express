a0f7ae094d558320031ca5b41d51f17c3f75aeb6
Dharshan S <dharshans805@gmail.com>
2025-07-24 19:47:54 +0530

    Res with HTML

--- Chap_3/src/server.js
+++ Chap_3/src/server.js
@@ -1,10 +1,24 @@
   
-  import express from 'express';
+import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
 
-  const app = express();
-  const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Get the file path of the current module
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name from the fie path
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 
-  app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-  })
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+})

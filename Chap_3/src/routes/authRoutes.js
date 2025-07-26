7710041c0810b14873fc99ab4cd850abf330e1a8
Dharshan S <dharshans805@gmail.com>
2025-07-26 22:38:19 +0530

    encryt and try catch for post register

--- Chap_3/src/routes/authRoutes.js
+++ Chap_3/src/routes/authRoutes.js
@@ -10,8 +10,28 @@ const router = express.Router();
 //for signup and register
 router.post('/register',(req,res)=>{
     const{username,password}=req.body; // v destruceted body to {username,password} to fetch the req data directly
-    console.log(username,password);
-    res.sendStatus(201);
+
+    //encrypt the password
+    const hashedPassword = bcrypt.hashSync(password,8); // 8 is the salt rounds
+
+    //store the user in the database
+    try{
+        const insertUser = db.prepare('INSERT INTO users(username,password) VALUES(?,?)')
+        const result = insertUser.run(username, hashedPassword); //saves this user in the database
+
+        //i want  to add their 1st todo for them
+        const defaultTodo =`Hello ${username} :) Add your first todo!`;
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES(?,?)`);
+        insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+        //creating the token
+        
+
+    }catch(err){
+        console.log(err.message);
+        return res.status(503).send('Error registering the user');
+    }
+
 });
 
 //for login

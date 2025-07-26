01e75251a1b3f2afce93bf75b74b5eded6c638e5
Dharshan S <dharshans805@gmail.com>
2025-07-26 17:12:04 +0530

    created the subsection in auth.js &  updated the server.js

---
+++ Chap_3/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import db from '../db.js';
+
+const router = express.router();
+
+//for signup and register
+router.post('/register',(req,res)=>{});
+
+//for login
+router.post('/login',(req,res)=>{});
+
+export default router;
--- Chap_3/src/server.js
+++ Chap_3/src/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import path, {dirname} from 'path';
 import {fileURLToPath} from 'url';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,8 @@ app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
+//Routes
+app.use('/auth',authRoutes);
 
 app.listen(PORT,()=>{
   console.log(`Server is running on port ${PORT}`);

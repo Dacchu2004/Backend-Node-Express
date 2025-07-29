9c5f15b6dc546188d22908627120467fbd998a1c
Dharshan S <dharshans805@gmail.com>
2025-07-29 14:05:18 +0530

    Create authMiddleware.js

---
+++ Chap_3/src/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+// This middleware will be used in the routes to protect them and get the user id from the token
+// It will be used in the todoRoutes.js file to get the todos for the logged in user
+// and also to create, update and delete todos for the logged in user
+import jwt from 'jsonwebtoken';
+
+function authMiddleware(req,res,next){
+    const token = req.hearders['authorization']; //get the token from the request headers
+    if(!token) {return res.status(401).json({message: "No Token Found!"})}
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(401).json({message:"Inavlid Token..!"})
+        }
+        req.userId = decoded.id; //set the user id in the request object
+        next(); //call the next middleware or route handler
+    }) 
+}
+
+export default authMiddleware;

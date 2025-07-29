9a250891eb4e62e9fcb61a609f0cbc0463e8e6e2
Dharshan S <dharshans805@gmail.com>
2025-07-29 13:14:32 +0530

    create get todos for user endpoint

--- Chap_3/src/routes/todoRoutes.js
+++ Chap_3/src/routes/todoRoutes.js
@@ -3,8 +3,12 @@ import db from '../db.js';
 
 const router = express.Router();
 
-//to get to-do's
-router.get('/',(req,res)=>{});
+//to get to-do's for logged in user
+router.get('/',(req,res)=>{
+    const getTodos= db.prepare(`SELECT * FROM todos WHERE user_id=?`);
+    const todos = getTodos.all(req.userId); //req.userId is set by the auth middleware, which is not shown here
+    res.json(todos); //send todos to the client
+});
 
 //to create/add new to-do's
 router.post('/',(req,res)=>{});
